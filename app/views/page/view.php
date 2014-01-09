7f29ff64ebe6848385de1568def232719d12b6e2
Raysmond <jiankunlei@126.com>
2014-01-10 02:43:04 +0800

    Add Duoshuo

--- app/views/page/view.php
+++ app/views/page/view.php
@@ -1,18 +1,19 @@
 <?php $self->setHeaderTitle($page->title); ?>
 
-<div class="page-header"><h1><?= RHtml::encode($page->title) ?></h1></div>
-<div class="page-content">
-    <?= $page->content ?>
-</div>
-<div class="page-meta">
-    ---------------- <br/>
-    Updated at <?= $page->updateTime ?>
-</div>
-<div class="actions">
-    <?php
-    if (Rays::isLogin() && Rays::user()->role === User::ADMIN) {
-        echo "Actions: ";
-        echo RHtml::linkAction('page', "Edit", "edit", $page->id);
-    }
-    ?>
-</div>
+    <div class="page-header"><h1><?= RHtml::encode($page->title) ?></h1></div>
+    <div class="page-content">
+        <?= $page->content ?>
+    </div>
+    <div class="page-meta">
+        ---------------- <br/>
+        Updated at <?= $page->updateTime ?>
+    </div>
+    <div class="actions">
+        <?php
+        if (Rays::isLogin() && Rays::user()->role === User::ADMIN) {
+            echo "Actions: ";
+            echo RHtml::linkAction('page', "Edit", "edit", $page->id);
+        }
+        ?>
+    </div>
+<?= $self->renderPartial("_common._duoshuo", null, true) ?>
